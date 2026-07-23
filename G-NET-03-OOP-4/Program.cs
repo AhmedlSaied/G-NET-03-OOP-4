0db128cc3f0abfaa469a679eac8e3ec626666bcb
AhmedlSaied <kledkled.17@gmail.com>
2026-07-24 00:35:09 +0300

    SUPPORTING CLASSES

--- G-NET-03-OOP-4/Program.cs
+++ G-NET-03-OOP-4/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace OOPAssignment04
+{
+    #region SUPPORTING CLASSES
+
+    public class Projector
+    {
+        public void Start() => Console.WriteLine("Projector started.");
+        public void Stop() => Console.WriteLine("Projector stopped.");
+    }
+
+    #endregion
+
+}
