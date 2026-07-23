0e2655bf40b56f1691c9294223bd12b9dc24638d
AhmedlSaied <kledkled.17@gmail.com>
2026-07-24 00:44:11 +0300

    PART 02 - QUESTION 2: CHILD TICKET CLASSES

--- G-NET-03-OOP-4/Program.cs
+++ G-NET-03-OOP-4/Program.cs
@@ -82,5 +82,65 @@ namespace OOPAssignment04
         #endregion
     }
     #endregion
+    #region PART 02 - QUESTION 2: CHILD TICKET CLASSES
+
+    public class StandardTicket : Ticket
+    {
+        public string SeatNumber { get; set; }
+
+        public StandardTicket(string movieName, decimal price, string seatNumber)
+            : base(movieName, price)
+        {
+            SeatNumber = seatNumber;
+        }
+
+        public override void PrintTicket()
+        {
+            base.PrintTicket();
+            Console.WriteLine();
+            Console.WriteLine($"  Seat: {SeatNumber}");
+        }
+    }
+
+    public class VIPTicket : Ticket
+    {
+        public bool LoungeAccess { get; set; }
+        public decimal ServiceFee { get; } = 50m;
+
+        public VIPTicket(string movieName, decimal price, bool loungeAccess)
+            : base(movieName, price)
+        {
+            LoungeAccess = loungeAccess;
+        }
+
+        public override void PrintTicket()
+        {
+            base.PrintTicket();
+            Console.WriteLine();
+            string loungeStr = LoungeAccess ? "Yes" : "No";
+            Console.WriteLine($"  Lounge: {loungeStr} | Service Fee: {ServiceFee:F0} EGP");
+        }
+    }
+
+    public class IMAXTicket : Ticket
+    {
+        public bool Is3D { get; set; }
+
+        public IMAXTicket(string movieName, decimal price, bool is3D)
+            : base(movieName, price + (is3D ? 30m : 0m))
+        {
+            Is3D = is3D;
+        }
+
+        public override void PrintTicket()
+        {
+            base.PrintTicket();
+            Console.WriteLine();
+            string is3DStr = Is3D ? "Yes" : "No";
+            Console.WriteLine($"  IMAX 3D: {is3DStr}");
+        }
+    }
+
+    #endregion
 }
 
