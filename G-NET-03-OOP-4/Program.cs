7adb8dc30808f05035e7bd3ccad3308efcba997f
AhmedlSaied <kledkled.17@gmail.com>
2026-07-24 00:41:39 +0300

    PART 02 - QUESTION 1: BASE TICKET CLASS

--- G-NET-03-OOP-4/Program.cs
+++ G-NET-03-OOP-4/Program.cs
@@ -12,4 +12,66 @@ namespace OOPAssignment04
 
     #endregion
 
+    #region PART 02 - QUESTION 1: BASE TICKET CLASS
+
+    public class Ticket
+    {
+        private static int totalTickets = 0;
+
+        private string movieName = "Unknown";
+        private decimal price = 1m;
+
+        public int TicketId { get; }
+
+        public string MovieName
+        {
+            get => movieName;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    movieName = value;
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public decimal PriceAfterTax => Price * 1.14m;
+
+        public Ticket(string movieName, decimal price)
+        {
+            TicketId = ++totalTickets;
+            MovieName = movieName;
+            Price = price;
+        }
+
+        public static int GetTotalTickets() => totalTickets;
+
+        #region Q1.b - SetPrice Overloading
+
+        public void SetPrice(decimal newPrice)
+        {
+            Price = newPrice;
+        }
+
+        public void SetPrice(decimal basePrice, decimal multiplier)
+        {
+            Price = basePrice * multiplier;
+        }
+
+        #endregion
+    }
+    #endregion
 }
+
